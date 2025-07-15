1bf3e487e2a2740dea3210e46e29c5882f779139
hakan <hbilgic1992@gmail.com>
2025-07-15 15:10:13 +0200

    fixbuild on docker error cycle

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-const { defineConfig } = require('vite');
-const { reactRouter } = require('@react-router/dev/vite');
-const tailwindcss = require('@tailwindcss/vite');
-const tsconfigPaths = require('vite-tsconfig-paths');
+import { defineConfig } from 'vite';
+import { reactRouter } from '@react-router/dev/vite';
+import tailwindcss from '@tailwindcss/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({ isSsrBuild }) => ({
   // build: {
